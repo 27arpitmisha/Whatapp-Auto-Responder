9ff9695bc629de4594c7561584ae24962648dc5f
Arpit Mishra <27arpitmishra@gmail.com>
2022-05-19 18:22:05 +0530

    What app bot

---
+++ server.js
@@ -0,0 +1,33 @@
+const qrcode = require('qrcode-terminal');
+const axios = require('axios');
+
+const { Client, MessageMedia } = require('whatsapp-web.js');
+const client = new Client();
+
+client.on('qr', qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+ client.on('message', async (message) => {
+     console.log(message.body.toLowerCase());
+	if(message.body.toLowerCase() === 'send dudes') {
+        const meme  = await axios(' https://meme-api.herokuapp.com/gimme').then(res => res.data)
+        
+        message.reply('you mean nudes ?');  
+        client.sendMessage(message.from, await MessageMedia.fromUrl(meme.url))
+	} else if (message.body.toLowerCase() === 'arpit'){
+        const contact = await message.getContact();
+        console.log(contact)
+        message.reply(' yo whats up' + contact);
+    } 
+});
+
+client.on('message', message => {
+	console.log(message.body);
+});
+
+client.initialize();
